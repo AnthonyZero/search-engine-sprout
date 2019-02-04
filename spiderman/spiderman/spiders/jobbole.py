c79198e1f6ee515673a4078204750697d4218463
Anthony <736252868@qq.com>
2019-02-04 17:44:50 +0800

    commit

--- spiderman/spiderman/spiders/jobbole.py
+++ spiderman/spiderman/spiders/jobbole.py
@@ -5,7 +5,9 @@ import scrapy
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/114536/']
 
     def parse(self, response):
+        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract()[0];
+        create_data = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].replace("·","").strip()
         pass
