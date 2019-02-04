ac94c1d2e25fff3048605a0ac244f697229342c1
Anthony <736252868@qq.com>
2019-02-04 23:27:34 +0800

    jobbole css

--- spiderman/spiderman/spiders/jobbole.py
+++ spiderman/spiderman/spiders/jobbole.py
@@ -8,17 +8,40 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/114536/']
 
     def parse(self, response):
-        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract()[0];
-        create_data = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].replace("·","").strip()
-        vote_num = response.xpath('//span[contains(@class,"vote-post-up")]/h10/text()').extract()[0]
-        collect_num = response.xpath('//span[contains(@class,"bookmark-btn")]/text()').extract()[0]
-        match_re = re.match(".*(\d+).*", collect_num)
-        favour_num = 0
+        # title = response.xpath('//div[@class="entry-header"]/h1/text()').extract()[0];
+        # create_data = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].replace("·","").strip()
+        # vote_num = response.xpath('//span[contains(@class,"vote-post-up")]/h10/text()').extract()[0]
+        # collect_num = response.xpath('//span[contains(@class,"bookmark-btn")]/text()').extract()[0]
+        # match_re = re.match(".*?(\d+).*", collect_num)
+        # favour_num = 0
+        # if match_re:
+        #     favour_num = match_re.group(1)
+        # comments_num = response.xpath('//a[@href="#article-comment"]/span/text()').extract()[0]
+        # match_re = re.match(".*?(\d+).*", comments_num) #正则获取字符串中的数字
+        # if match_re:
+        #     comments_num = match_re.group(1)
+        #
+        # content = response.xpath('//div[@class="entry"]').extract()[0]
+        # tag_selecter = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
+        # tag_list = [element for element in tag_selecter if not element.strip().endswith('评论')]
+        # tags = ",".join(tag_list) #标签
+
+        # 通常css选择器提取数据
+        title = response.css('.entry-header h1::text').extract_first()
+        create_data = response.css('p.entry-meta-hide-on-mobile::text').extract_first().replace("·","").strip()
+        praise_num = response.css('.vote-post-up h10::text').extract_first() #点赞数
+        fav_num = response.css('.bookmark-btn::text').extract_first() #收藏数
+        match_re = re.match(".*?(\d+).*", fav_num)
         if match_re:
-            favour_num = match_re.group(1)
-        comments_num = response.xpath('//a[@href="#article-comment"]/span/text()').extract()[0]
-        match_re = re.match(".*(\d+).*", comments_num) #正则获取字符串中的数字
+            fav_num = match_re.group(1)
+        else:
+            fav_num = 0
+        comments_num = response.css('a[href="#article-comment"] span::text').extract_first() # 评论数
+        match_re = re.match(".*?(\d+).*", comments_num)  # 正则获取字符串中的数字
         if match_re:
             comments_num = match_re.group(1)
-
+        content = response.css('div.entry').extract_first() # 正文
+        tag_selecter = response.css('p.entry-meta-hide-on-mobile a::text').extract()
+        tag_list = [element for element in tag_selecter if not element.strip().endswith('评论')]
+        tags = ",".join(tag_list)  # 标签
         pass
