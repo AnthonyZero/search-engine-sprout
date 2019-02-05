35951f965ee61de0296d9dd4fc2774691d7349a2
Anthony <736252868@qq.com>
2019-02-05 22:23:19 +0800

    获取下载后 返回的相对路径

--- spiderman/spiderman/settings.py
+++ spiderman/spiderman/settings.py
@@ -66,7 +66,8 @@ ROBOTSTXT_OBEY = False
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    'spiderman.pipelines.SpidermanPipeline': 300,
-   'scrapy.pipelines.images.ImagesPipeline': 1
+   #'scrapy.pipelines.images.ImagesPipeline': 1
+   'spiderman.pipelines.ArticleImagePipeline': 1
 }
 IMAGES_URLS_FIELD = "front_image_url"
 project_dir = os.path.abspath(os.path.dirname(__file__))
