5c73f15796357affc2decfa75c286af30beee9f0
Anthony <736252868@qq.com>
2019-02-08 22:23:17 +0800

    将.../spiderman/spiderman 加入到python path中

--- spiderman/spiderman/settings.py
+++ spiderman/spiderman/settings.py
@@ -77,6 +77,10 @@ IMAGES_URLS_FIELD = "front_image_url" # 指定图片下载URL
 project_dir = os.path.abspath(os.path.dirname(__file__))
 IMAGES_STORE = os.path.join(project_dir, "images")  # 图片存放路径
 
+# 将.../spiderman/spiderman 加入到python path中
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.join(BASE_DIR, 'spiderman'))
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
