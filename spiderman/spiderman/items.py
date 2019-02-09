7cea15b54f2d460aca3c6f0ad994d04f9c729930
Anthony <736252868@qq.com>
2019-02-09 20:14:19 +0800

    lagou item handler

--- spiderman/spiderman/items.py
+++ spiderman/spiderman/items.py
@@ -10,6 +10,7 @@ from scrapy.loader.processors import MapCompose,Join,TakeFirst
 from datetime import datetime
 import re
 from scrapy.loader import ItemLoader
+from w3lib.html import remove_tags
 
 
 class SpidermanItem(scrapy.Item):
@@ -90,6 +91,16 @@ class LagouJobItemLoader(ItemLoader):
     #自定义itemloader
     default_output_processor = TakeFirst()
 
+def remove_splash(value):
+    #去掉多余的斜线
+    return value.replace("/","")
+
+# 处理拉钩的地址
+def handler_addr(value):
+    addr_list = value.split("\n")
+    addr_list = [item.strip() for item in addr_list if item.strip() != '查看地图']
+    return "".join(addr_list)
+
 class LagouJobItem(scrapy.Item):
     #拉勾网职位信息
     title = scrapy.Field()
@@ -97,24 +108,24 @@ class LagouJobItem(scrapy.Item):
     url_object_id = scrapy.Field()
     salary = scrapy.Field()
     job_city = scrapy.Field(
-
+        input_processor = MapCompose(remove_splash)
     )
     work_years = scrapy.Field(
-
+        input_processor=MapCompose(remove_splash)
     )
     degree_need = scrapy.Field(
-
+        input_processor=MapCompose(remove_splash)
     )
     job_type = scrapy.Field()
     publish_time = scrapy.Field()
     job_advantage = scrapy.Field()
     job_desc = scrapy.Field()
     job_addr = scrapy.Field(
-
+        input_processor=MapCompose(remove_tags, handler_addr)
     )
     company_name = scrapy.Field()
     company_url = scrapy.Field()
     tags = scrapy.Field(
-
+        output_processor=Join(",")
     )
     crawl_time = scrapy.Field()
