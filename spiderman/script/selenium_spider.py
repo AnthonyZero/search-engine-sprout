1978ba22a01399146573e0c34c26ece6fc5a64ca
Anthony <736252868@qq.com>
2019-02-24 21:11:07 +0800

    知乎登陆

--- spiderman/script/selenium_spider.py
+++ spiderman/script/selenium_spider.py
@@ -11,10 +11,20 @@
 from selenium import webdriver
 from scrapy.selector import Selector
 
+# 知乎登陆
+# browser = webdriver.Chrome(executable_path='F:/tmp/chromedriver.exe')
+# browser.get('https://www.zhihu.com/signup?next=%2F')
+# print(browser.page_source)
+# # 点击登陆tab
+# browser.find_element_by_css_selector('.SignContainer-switch span').click()
+# browser.find_element_by_css_selector(".SignFlow-accountInput input[name='username']").send_keys('username')
+# browser.find_element_by_css_selector(".SignFlow-password input[name='password']").send_keys('password')
+# browser.find_element_by_css_selector('.SignFlow-submitButton').click()
+
 # 获取天猫商品促销价
-browser = webdriver.Chrome(executable_path='F:/tmp/chromedriver.exe')
-browser.get('https://detail.tmall.com/item.htm?spm=a230r.1.14.3.yYBVG6&id=538286972599&cm_id=140105335569ed55e27b&abbucket=15&sku_properties=10004:709990523;5919063:6536025')
-print(browser.page_source)
+# browser = webdriver.Chrome(executable_path='F:/tmp/chromedriver.exe')
+# browser.get('https://detail.tmall.com/item.htm?spm=a230r.1.14.3.yYBVG6&id=538286972599&cm_id=140105335569ed55e27b&abbucket=15&sku_properties=10004:709990523;5919063:6536025')
+# print(browser.page_source)
 # selector = Selector(text=browser.page_source)
 # goods_value = selector.css('.tm-price-panel .tm-price::text').extract_first()
 # print("该商品原价为:", goods_value)
